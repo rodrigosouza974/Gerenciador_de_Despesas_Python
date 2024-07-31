88f5fc7bbfdb5847599e24b84edd2a96a28d9e6a
Rodrigo souza <124813706+rodrigosouza974@users.noreply.github.com>
2024-07-30 22:17:20 -0300

    sistema inicial

--- Gerenciador_Despesas.py
+++ Gerenciador_Despesas.py
@@ -22,15 +22,128 @@ Sugestões para implementação:
 
 Isso deve fornecer um desafio interessante para você elaborar um projeto completo em Python, aplicando conceitos como entrada e saída de dados, estruturas de dados, funções e tratamento de erros. Se precisar de ajuda ou tiver alguma dúvida durante a implementação, estou aqui para ajudar!
 '''
+from datetime import datetime
 
-#despesas diárias, como descrição da despesa e valor gasto.
 
-# função input
-nome_despesa = input("qual o nome da despesa:")
+def registrar_data_arquivo(func):
+    def wrapper(*args, **kwargs):
+        data_atual = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        with open('data_execucao.txt', 'a') as arquivo:
+            arquivo.write(f'{data_atual} - {func.__name__} chamada\n')
+        return func(*args, **kwargs)
+    return wrapper
 
-despesa_descricao = input("qual descrição deseja adicionar para essa despesa:")
 
-valor_despesa = input("qual o valor da despesa:")
+class Despesa:
+    def __init__(self, categoria, descricao, valor):
+        self.categoria = categoria
+        self.descricao = descricao
+        self.valor = valor
 
-# função print
-print(f"olá {nome}")
+    def __str__(self):
+        return f"Data: {self.data}, Categoria: {self.categoria}, Descrição: {self.descricao}, Valor: {self.valor}"
+
+
+class ListaDeDespesas:
+    def __init__(self):
+        self.despesas = []
+
+    def adicionar_despesa(self, despesa):
+        self.despesas.append(despesa)
+        print(f'Despesa adicionada: {despesa}')
+
+    @registrar_data_arquivo
+    def alterar_despesa(self, indice, nova_categoria=None, nova_descricao=None, novo_valor=None):
+        if 0 <= indice < len(self.despesas):
+            despesa = self.despesas[indice]
+            if nova_categoria:
+                despesa.categoria = nova_categoria
+            if nova_descricao:
+                despesa.descricao = nova_descricao
+            if novo_valor:
+                despesa.valor = novo_valor
+            print(f'Despesa alterada: {despesa}')
+        else:
+            print('Índice de despesa inválido.')
+ 
+    @registrar_data_arquivo
+    def excluir_despesa(self, indice):
+        if 0 <= indice < len(self.despesas):
+            despesa_removida = self.despesas.pop(indice)
+            print(f'Despesa removida: {despesa_removida}')
+        else:
+            print('Índice de despesa inválido.')
+
+    @registrar_data_arquivo
+    def listar_despesas(self):
+        for despesa in self.despesas:
+            print(despesa)
+
+    def __str__(self):
+        return '\n'.join(str(despesa) for despesa in self.despesas)
+
+
+def menu():
+    print("""
+==================================================
+                Gestor de Despesas
+==================================================
+
+Bem-vindo à nossa aplicação! Aqui estão algumas
+informações importantes:
+
+    1. Adicionar despesa
+    2. Alterar despesa
+    3. Excluir despesa
+    4. Visualizar despesas
+    0. Sair
+
+Por favor, escolha uma das opções acima para
+continuar.
+
+==================================================
+""")
+
+
+def Sistema_Gerenciamento():
+    menu()
+    lista_de_despesas = ListaDeDespesas()
+
+    while True:
+        try:
+            opcao = int(input("Escolha uma opção: "))
+
+            if opcao == 1:
+                categoria = input("Categoria da despesa: ")
+                descricao = input("Descrição da despesa: ")
+                valor = float(input("Valor da despesa: "))
+                nova_despesa = Despesa(categoria, descricao, valor)
+                lista_de_despesas.adicionar_despesa(nova_despesa)
+
+            elif opcao == 2:
+                indice = int(input("Índice da despesa a alterar: "))
+                nova_categoria = input("Nova categoria (ou deixe em branco para manter): ")
+                nova_descricao = input("Nova descrição (ou deixe em branco para manter): ")
+                novo_valor = input("Novo valor (ou deixe em branco para manter): ")
+                novo_valor = float(novo_valor) if novo_valor else None
+                lista_de_despesas.alterar_despesa(indice, nova_data, nova_categoria, nova_descricao, novo_valor)
+
+            elif opcao == 3:
+                indice = int(input("Índice da despesa a excluir: "))
+                lista_de_despesas.excluir_despesa(indice)
+
+            elif opcao == 4:
+                lista_de_despesas.listar_despesas()
+
+            elif opcao == 0:
+                break
+
+        except ValueError:
+            print("Por favor, insira um número válido.")
+        print()
+        # Exibir o menu novamente após cada operação para referência
+        menu()
+
+
+if __name__ == "__main__":
+    Sistema_Gerenciamento()
